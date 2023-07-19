0437066e1b7ab3b1196ed845eb86bea83617b874
gcoleman72 <g.coleman@aims.gov.au>
2023-07-20 08:13:38 +1000

    some extra parameters

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='photoenhancer',
     packages=['photoenhancer', 'photoenhancer.dehaze', 'photoenhancer.utils'],
-    version='1.1.9',
+    version='1.1.10',
     description='aims_reefscan_photo_enhancer',
     author='AIMS',
     license='MIT',
